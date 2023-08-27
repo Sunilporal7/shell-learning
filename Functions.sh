cb0461c84044eb9a4c427cd4a7170d8d8263d0e7
sunil <sunilporal713@gmail.com>
2023-08-27 14:04:24 +0530

    new lines added

---
+++ Functions.sh
@@ -0,0 +1,9 @@
+# Functions are like variables
+#variables - when we assign a name to a set of data then it is variable
+#Functions -  when we assign a name to a set of commands then it is function
+
+#Function Declare
+example(){
+  echo this is a example function
+}
+ example
